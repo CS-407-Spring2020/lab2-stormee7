90b0fe1b7ed3747da8ae96664b79e88d0102dafd
stormee7 <stormowski@wisc.edu>
2020-02-07 15:36:21 -0600

    First Commit

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
+import android.view.View;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public void clickFunction(View view){
+
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
